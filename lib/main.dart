71da97e4bbcaaa39a8d90c71d9fd51be6a2c2922
thumb2086 <cpxru83@gmail.com>
2026-07-26 16:54:20 +0800

    v2.6.44 - bring existing instance to front

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,37 @@
+import 'dart:ffi';
 import 'dart:io';
+import 'package:ffi/ffi.dart';
 import 'package:flutter/material.dart';
+import 'package:win32/win32.dart';
 import 'app.dart';
 import 'services/config_service.dart';
 import 'services/groq_service.dart';
 import 'services/i18n.dart';
+
+int _foundHwnd = 0;
+
+int _enumFindFlutter(int h, int _) {
+  final len = GetWindowTextLength(h) + 1;
+  final buf = calloc<Uint16>(len);
+  GetWindowText(h, buf.cast<Utf16>(), len);
+  final title = buf.cast<Utf16>().toDartString();
+  calloc.free(buf);
+  if (title.contains('Playlist') || title.contains('播放清單')) {
+    _foundHwnd = h;
+    return FALSE;
+  }
+  return TRUE;
+}
+
+void _bringToFront() {
+  _foundHwnd = 0;
+  final callback = Pointer.fromFunction<WNDENUMPROC>(_enumFindFlutter, FALSE);
+  EnumWindows(callback, 0);
+  if (_foundHwnd != 0) {
+    if (IsIconic(_foundHwnd) != 0) ShowWindow(_foundHwnd, SW_RESTORE);
+    SetForegroundWindow(_foundHwnd);
+  }
+}
 
 RandomAccessFile? _lockFile;
 
@@ -13,6 +41,7 @@ void _ensureSingleInstance() {
     _lockFile = File(lockPath).openSync(mode: FileMode.write);
     _lockFile!.lockSync();
   } catch (_) {
+    _bringToFront();
     exit(0);
   }
 }
